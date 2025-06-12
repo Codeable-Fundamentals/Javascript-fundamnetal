50769477b8b9b61bb4e695f75d67ca27d020347a
Grecia Delgado <greciadm2596@gmail.com>
2025-06-11 19:08:28 -0500

    add changes

--- week1/game.js
+++ week1/game.js
@@ -11,13 +11,23 @@ c = b;
 // console.log("Valor de b :", b) // Gonzalo
 // console.log("Valor de c :", c) // Gonzalo
 
-
-
 let x = 100;
 x = x + 50;
 let y = x + 200;
 y = y + x;
 x = x - y;
 
-console.log(x) // -350
-console.log(y) // 500
+console.log(x); // -350
+console.log(y); // 500
+
+// let let = 1525
+
+function sumar() {
+  var num1 = 1434;
+  let num2 = 2;
+  console.log("Valor: ", num1);
+
+  return num1 + num2;
+}
+
+console.log("Ejecutando funcion: ", sumar());
