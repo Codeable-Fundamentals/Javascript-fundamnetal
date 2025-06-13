9692f338daee8d9c714a0bac601fc2323b32cb47
Estuardo Franco <estuardofrancop@outlook.es>
2025-06-13 18:58:07 -0500

    tarea Estuardo Franco

---
+++ week1/funcion/tarea/estuardofranco.js
@@ -0,0 +1,34 @@
+// TAREA: Creador de Estudiantes
+function crearEstudiante(nombre, edad, carrera) {
+    // Retorna un objeto estudiante con:
+    // - Las propiedades recibidas
+    // - Una propiedad 'activo' que siempre sea true
+    // - Una propiedad 'promedio' que inicie en 0
+    return {name: nombre, age: edad, carreer: carrera, activo: true, promedio: 0}
+}
+
+function actualizarPromedio(estudiante, nuevaCalificacion) {
+    // Actualiza el promedio del estudiante
+    // (Para simplificar, solo reemplaza el promedio anterior)
+    estudiante.promedio = nuevaCalificacion;
+    return "Promedio actualizado";
+}
+
+// Prueba:
+let estudiante1 = crearEstudiante("Carlos", 20, "Ingeniería");
+console.log(estudiante1); // aqui empezara con la propiedad promedio en 0
+// {
+//   name: "Carlos", 
+//   age: 20, 
+//   carrera: "Ingeniería", 
+//   promedio: 0
+// }
+
+console.log(actualizarPromedio(estudiante1, 85));
+console.log(estudiante1); // aqui la propiedad pormedio sera 85
+// {
+//   name: "Carlos", 
+//   age: 20, 
+//   carrera: "Ingeniería", 
+//   promedio: 85
+// }
