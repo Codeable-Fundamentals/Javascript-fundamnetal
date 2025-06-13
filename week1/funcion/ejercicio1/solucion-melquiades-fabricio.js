12d14e26cfbabd3a60a9d0de8dd3ce187a4597cb
mel-yopla <myoplaf@gmail.com>
2025-06-12 20:35:32 -0500

    Solución Ejercicio Melquiades - Fabricio

---
+++ week1/funcion/ejercicio1/solucion-melquiades-fabricio.js
@@ -0,0 +1,27 @@
+function puedeConducir(edad, tieneLicencia) {
+  // Debe ser mayor de 18 años Y tener licencia
+  // Retorna: "Puede conducir" o "No puede conducir"
+  // Usa operadores lógicos (&&, ||)
+
+  // SOLUCIÓN 1
+  // edad >= 18 && tieneLicencia
+  //   ? console.log("Puede conducir")
+  //   : console.log(" No puede conducir");
+
+  // SOLUCIÓN 2 - ÓPTIMA
+  return edad >= 18 && tieneLicencia ? "Puede conducir" : "No puede conducir";
+
+  // SOLUCIÓN 3
+  // let respuesta;
+  // if (edad >= 18 && tieneLicencia) {
+  //   respuesta = "Puede Conducir";
+  // } else {
+  //   respuesta = "No puede conducir";
+  // }
+  // return respuesta;
+}
+
+// Pruebas:
+console.log(puedeConducir(20, true)); // "Puede conducir"
+console.log(puedeConducir(17, true)); // "No puede conducir"
+console.log(puedeConducir(25, false)); // "No puede conducir"
--- week1/funcion/ejercicio1/test-prueba-profesora.js
+++ week1/funcion/ejercicio1/test-prueba-profesora.js
@@ -1,10 +1,10 @@
 function puedeConducir(edad, tieneLicencia) {
-    // Debe ser mayor de 18 años Y tener licencia
-    // Retorna: "Puede conducir" o "No puede conducir"
-    // Usa operadores lógicos (&&, ||)
+  // Debe ser mayor de 18 años Y tener licencia
+  // Retorna: "Puede conducir" o "No puede conducir"
+  // Usa operadores lógicos (&&, ||)
 }
 
 // Pruebas:
-console.log(puedeConducir(20, true));   // "Puede conducir"
-console.log(puedeConducir(17, true));   // "No puede conducir"
-console.log(puedeConducir(25, false));  // "No puede conducir"
+console.log(puedeConducir(20, true)); // "Puede conducir"
+console.log(puedeConducir(17, true)); // "No puede conducir"
+console.log(puedeConducir(25, false)); // "No puede conducir"
