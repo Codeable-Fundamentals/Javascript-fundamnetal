c9c5f58e15e0f6a7815537152a27c4255a7b6956
vi18xd <jenni_jc9@hotmail.com>
2025-06-12 20:38:40 -0500

    resolucion-victoria-estuardo-carlosV2

--- week1/funcion/ejercicio1/test-victoria-estuardo-carlos.js
+++ week1/funcion/ejercicio1/test-victoria-estuardo-carlos.js
@@ -2,6 +2,7 @@ function puedeConducir(edad, tieneLicencia) {
   // Debe ser mayor de 18 años Y tener licencia
   // Retorna: "Puede conducir" o "No puede conducir"
   // Usa operadores lógicos (&&, ||)
+
   if (
     typeof edad !== "number" ||
     typeof tieneLicencia !== "boolean" ||
@@ -9,14 +10,14 @@ function puedeConducir(edad, tieneLicencia) {
     edad > 120
   ) {
     return "Datos inválidos";
-  } else if (edad >= 18 && tieneLicencia) {
-    return "Puede conducir";
   } else {
-    return "No puede conducir";
+    return edad >= 18 && tieneLicencia ? "Puede conducir" : "No puede conducir";
   }
 }
 
 // Pruebas:
-console.log(puedeConducir("gggg", true)); // "Puede conducir"
-console.log(puedeConducir(170, true)); // "No puede conducir"
+console.log(puedeConducir(18, true)); // "Datos inválidos"
+console.log(puedeConducir(170, true)); // "Datos inválidos"
 console.log(puedeConducir(25, false)); // "No puede conducir"
+
+
