63f3ca6f8f16ca1ba853b3fb101ed414f4740417
Krascar <oscarmamani0079@gmail.com>
2025-06-13 11:23:05 -0500

    tarea_oscar

--- week1/funcion/tarea/tarea-oscar.js
+++ week1/funcion/tarea/tarea-oscar.js
@@ -1,8 +1,9 @@
-function crearEstudiante(nombre, edad, carrera) {
+function crearEstudiante(nombre, edad, carrera, promedio) {
         let Estudiante ={
         nombre : nombre,
         edad : edad,
         carrera : carrera,
+        promedio : promedio,
     }
     return Estudiante
 }
@@ -12,13 +13,49 @@ function actualizarPromedio(estudiante, nuevaCalificacion) {
         promedio : nuevaCalificacion,
     }
     estudiante.promedio = nuevaCalificacion;
+
     return calificion , "Promedio actualizado";
 }
 console.log("------------PRIMERA PRUEBA------------")
-let estudiante1 = crearEstudiante("Carlos", 20, "Ingeniería");
-console.log(estudiante1);
+
+let estudiante1 = crearEstudiante("Carlos", 20, "Ingeniería", 0);
+console.log("\n",
+    "nombre " + estudiante1.nombre + "\n",
+    "edad " + estudiante1.edad + "\n",
+    "carrera " + estudiante1.carrera + "\n",
+    "promedio " + estudiante1.promedio + "\n",
+);
 
 console.log("------------SEGUNDA PRUEBA------------")
 
 console.log(actualizarPromedio(estudiante1, 85));
-console.log(estudiante1);
+console.log("\n",
+    "nombre " + estudiante1.nombre + "\n",
+    "edad " + estudiante1.edad + "\n",
+    "carrera " + estudiante1.carrera + "\n",
+    "promedio " + estudiante1.promedio + "\n",
+);
+
+
+
+
+
+
+
+// console.log("------------PRIMERA PRUEBA------------")
+// console.log("\n")
+
+// let estudiante1 = crearEstudiante("Carlos", 20, "Ingeniería", 0);
+// console.log("nombre " + estudiante1.nombre);
+// console.log("edad " + estudiante1.edad);
+// console.log("carrera " + estudiante1.carrera);
+// console.log("promedio " + estudiante1.promedio);
+
+// console.log("------------SEGUNDA PRUEBA------------")
+
+// console.log(actualizarPromedio(estudiante1, 85));
+
+// console.log("nombre " + estudiante1.nombre);
+// console.log("edad " + estudiante1.edad);
+// console.log("carrera " + estudiante1.carrera);
+// console.log("promedio " + estudiante1.promedio);
