455aae32a292ca92e18f4847a5110db8b4efa423
Grecia Delgado <greciadm2596@gmail.com>
2025-06-13 20:26:32 -0500

    add soluciones

---
+++ week1/igualdad-en-JS/ejercicio-en-clase/prueba.js
@@ -0,0 +1,22 @@
+// Ejercicio: Validar datos de una persona
+// Quiero que creen una funcion llamada `validarDatos` que recibaa y valide lo siguiente:
+// - 1. Si tiene nombre
+// - 2. si tiene edad 
+
+// si tiene ambos (Nombre y edad) , entonces devuelve true
+// Si le falta alguna de ellas, entonces deveulve false
+
+
+
+
+const persona1 = { nombre: "Ana", edad: 25 };
+const persona2 = { nombre: "Luis" };
+const persona3 = { edad: 30 };
+const persona4 = { nombre: null, edad: 25 };
+const persona5 = { };
+
+console.log(validarDatos(persona1)); // true
+console.log(validarDatos(persona2)); // false
+console.log(validarDatos(persona3)); // false
+console.log(validarDatos(persona4)); // false
+console.log(validarDatos(persona5)); // false
