056a3127b02ecdba4ed5d13e8b5d743f00cb8647
Estuardo Franco <estuardofrancop@outlook.es>
2025-06-13 21:16:37 -0500

    ejercicio Melquiades Estuardo formateado

--- week1/igualdad-en-JS/ejercicio-en-clase/prueba-melquiades-estuardo.js
+++ week1/igualdad-en-JS/ejercicio-en-clase/prueba-melquiades-estuardo.js
@@ -6,8 +6,11 @@
 // si tiene ambos (Nombre y edad) , entonces devuelve true
 // Si le falta alguna de ellas, entonces deveulve false
 
-function validarDatos (persona) {
-    if (persona.nombre !== undefined && persona.nombre !== null && persona.edad !== null && persona.edad !== undefined) {
+function validarDatos(persona) {
+    if (persona.nombre !== undefined &&
+        persona.nombre !== null &&
+        persona.edad !== null &&
+        persona.edad !== undefined) {
         return true;
     } else {
         return false
@@ -19,7 +22,7 @@ const persona1 = { nombre: "Ana", edad: 25 };
 const persona2 = { nombre: "Luis" };
 const persona3 = { edad: 30 };
 const persona4 = { nombre: null, edad: 25 };
-const persona5 = { };
+const persona5 = {};
 const persona6 = { nombre: null, edad: NaN };
 
 console.log(validarDatos(persona1)); // true
