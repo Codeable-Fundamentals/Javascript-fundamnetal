9fcab3be7218b267fc9a983ec8ac87cd35c65940
Grecia Delgado <greciadm2596@gmail.com>
2025-06-18 21:04:53 -0500

    feat: add console.log

--- week2/objects/trabajo-en-grupo/ejercicio.js
+++ week2/objects/trabajo-en-grupo/ejercicio.js
@@ -2,3 +2,8 @@
 // Cada método debe recibir dos números y retornar el resultado
 
 // const calculadora = {}
+
+console.log("SUMA: ", calculadora.sumar(3, 5)); // 8
+console.log("SUMA: ", calculadora.restar(13, 23)); // -10
+console.log("SUMA: ", calculadora.multiplicar(3, 5)); // 15
+console.log("SUMA: ", calculadora.dividir(25 / 5)); // 5
